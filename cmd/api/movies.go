f5ef796cfe074e1b3b631c6821771ce4d474a434
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-08-26 16:59:01 +0530

    Feature: Added race condition logic - added race condition handling in updateMovieHandler - added validations for the errors in updateMovieHandlers and errors.go - changed the method PUT to PATCH for updateMovieHandler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"greenlight.codewithyash.dev/internal/data"
 	"greenlight.codewithyash.dev/internal/validator"
@@ -97,10 +98,17 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	if r.Header.Get("X-Expected-Version") !="" {
+		if strconv.Itoa(int(movie.Version)) != r.Header.Get("X-Expected-Version") {
+			app.editConflictResponse(w, r)
+			return
+		}
+	}
+
 	var input struct {
-		Title 	string			`json:"title"`
-		Year 	int32			`json:"year"`
-		Runtime data.Runtime 	`json:"runtime"`
+		Title 	*string			`json:"title"`
+		Year 	*int32			`json:"year"`
+		Runtime *data.Runtime 	`json:"runtime"`
 		Genres  []string	 	`json:"genres"`
 	}
 
@@ -110,10 +118,18 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	v := validator.New()
 
@@ -124,7 +140,12 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
 
