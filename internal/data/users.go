e3ec98a56b67fef70913e43bdc0a4db7da7a1db1
Yash Gupta <158163628+YashGupta2116@users.noreply.github.com>
2026-08-29 18:39:53 +0530

    Fix: Fixed the INSERT user method

--- internal/data/users.go
+++ internal/data/users.go
@@ -94,12 +94,12 @@ func (m *UserModel) Insert(user *User) error {
 	VALUES ($1, $2, $3, $4)
 	RETURNING id, created_at, version`
 
-	args := []any{user.Name, user.CreatedAt, user.Version}
+	args := []any{user.Name, user.Email, user.Password.hash, user.Activated}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
 	defer cancel()
 
-	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Name, &user.CreatedAt, &user.Version)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
 	if err != nil {
 		switch {
 		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
